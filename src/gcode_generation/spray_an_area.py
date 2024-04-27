ee5aefd63543c240888c4b9c35aeecf896265f48
TomChristensen474 <82264960+TomChristensen474@users.noreply.github.com>
2024-04-27 15:43:21 +0100

    minor tweaks to gcode generation script

--- src/gcode_generation/spray_an_area.py
+++ src/gcode_generation/spray_an_area.py
@@ -21,25 +21,26 @@ class Area:
 
 # definition of the geometrical parameters. All dimentions are expressed in mm
 # Number_Slides = 1                  # number of slides to coat at once
-START = Point(x=50, y=50)  # Coordinates of the center of the sample
-TOOL_OFFSET = Position(x=0, y=50, z=0)  # offset of airbrush from the print head
+START = Point(x=25, y=90)  # Coordinates of the center of the sample
+# TOOL_OFFSET = Position(x=10, y=5, z=0)  # offset of airbrush from the print head
 # Orientation_Slide = 0              # direction of the slide's major dimention. 0 for X, 1 for Y
 # Dimention_Slide = (75.5,25.5)       # dimention of one slide, major dimention first
 # Positioning_uncertainty = 2         # imprecision on the slides positioning
 
 # definition of the spraying parameter geometry
 SPRAY_DIAMGETER = 23  # diameter of the spray
-HATCH = 12  # distance between two spraying lines
-SPRAY_HEIGHT = 100  # distance between the sample and the spray head
+HATCH = 8  # distance between two spraying lines
+SPRAY_HEIGHT = 80  # distance between the sample and the spray head
 PURGING_OFFSET = 25  # distance from the coating area to begin spaying
 # Number_layers = 40                 # Number of layers (ie scans)
 STAGE_TEMP = 50  # hotplate temperature in degree
 FEEDRATE = 4800  # feedrate, displacement speed during spraying
 
-LAYER_DELAY = 30  # Delay between two layers to let the water evaporate
+LAYER_DELAY = 45  # Delay between two layers to let the water evaporate
+SPRAY_DELAY_REFILL = 2  # Delay to refill cup
 SPRAY_DELAY = 2  # Delay between two spray lines
 
-SPRAY_AREA = Area(width=100, height=100)
+SPRAY_AREA = Area(width=125, height=120)
 LAYERS = 1
 
 gcode = GCODE_writer('area_spray.gcode')
@@ -63,10 +64,11 @@ for i in range(LAYERS):
     for i in range(iterations):                                             # iterate over the number of lines needed to cover spray area
         gcode.Show(f'layer {i} ongoing')
         
-        gcode.print_G1_line(line_start.x, line_start.y, SPRAY_HEIGHT)         # move to the starting point of layer
+        gcode.print_G1_line(line_start.x, line_start.y, SPRAY_HEIGHT)         # move to the starting point of line
         
-        gcode.Spray()
         gcode.Delay(SPRAY_DELAY)
+        gcode.Spray()
+        # gcode.Delay(1) # time for servo to activate
         gcode.print_G1_line(line_start.x + SPRAY_AREA.width, line_start.y, SPRAY_HEIGHT)  # move to the end of the line
         gcode.Stop_Spray()                                                 # Stop the spray
         gcode.Delay(SPRAY_DELAY)
